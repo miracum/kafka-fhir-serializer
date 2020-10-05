6708201ab12b3dbc80161e9f24befbd215253829
Christian Gulden <christian.gulden@fau.de>
2020-10-05 21:57:40 +0200

    style: applied Google Java Code style

--- src/main/java/org/miracum/kafka/serializers/KafkaFhirDeserializer.java
+++ src/main/java/org/miracum/kafka/serializers/KafkaFhirDeserializer.java
@@ -1,43 +1,40 @@
 package org.miracum.kafka.serializers;
 
+import static org.miracum.kafka.serializers.KafkaFhirSerde.CONFIG_FHIR_CONTEXT_KEY;
+
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.parser.IParser;
+import java.io.ByteArrayInputStream;
+import java.util.Map;
 import org.apache.kafka.common.header.Headers;
 import org.apache.kafka.common.serialization.Deserializer;
 import org.hl7.fhir.instance.model.api.IBaseResource;
 
-import java.io.ByteArrayInputStream;
-import java.util.Map;
-
-import static org.miracum.kafka.serializers.KafkaFhirSerde.CONFIG_FHIR_CONTEXT_KEY;
-
 public class KafkaFhirDeserializer implements Deserializer<IBaseResource> {
-    private IParser fhirParser = FhirContext.forR4().newJsonParser();
+  private IParser fhirParser = FhirContext.forR4().newJsonParser();
 
-    @Override
-    public void configure(Map<String, ?> configs, boolean isKey) {
-        if (configs.containsKey(CONFIG_FHIR_CONTEXT_KEY)) {
-            var context = (FhirContext) configs.get(CONFIG_FHIR_CONTEXT_KEY);
-            this.fhirParser = context.newJsonParser();
-        }
+  @Override
+  public void configure(Map<String, ?> configs, boolean isKey) {
+    if (configs.containsKey(CONFIG_FHIR_CONTEXT_KEY)) {
+      var context = (FhirContext) configs.get(CONFIG_FHIR_CONTEXT_KEY);
+      this.fhirParser = context.newJsonParser();
+    }
+  }
+
+  @Override
+  public IBaseResource deserialize(String topic, byte[] data) {
+    if (data == null) {
+      return null;
     }
 
-    @Override
-    public IBaseResource deserialize(String topic, byte[] data) {
-        if (data == null) {
-            return null;
-        }
+    return fhirParser.parseResource(new ByteArrayInputStream(data));
+  }
 
-        return fhirParser.parseResource(new ByteArrayInputStream(data));
-    }
+  @Override
+  public IBaseResource deserialize(String topic, Headers headers, byte[] data) {
+    return deserialize(topic, data);
+  }
 
-    @Override
-    public IBaseResource deserialize(String topic, Headers headers, byte[] data) {
-        return deserialize(topic, data);
-    }
-
-    @Override
-    public void close() {
-
-    }
+  @Override
+  public void close() {}
 }
--- src/test/java/org/miracum/kafka/serializers/KafkaFhirSerdeTest.java
+++ src/test/java/org/miracum/kafka/serializers/KafkaFhirSerdeTest.java
@@ -3,17 +3,18 @@
  */
 package org.miracum.kafka.serializers;
 
-import org.hl7.fhir.r4.model.Patient;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.hl7.fhir.r4.model.Patient;
+import org.junit.jupiter.api.Test;
+
 class KafkaFhirSerdeTest {
-    @Test
-    void serialize_withEmptyPatient_returnsNonEmptyBytes() {
-        var sut = new KafkaFhirSerde();
+  @Test
+  void serialize_withEmptyPatient_returnsNonEmptyBytes() {
+    var sut = new KafkaFhirSerde();
 
-        var bytes = sut.serializer().serialize(null, new Patient());
+    var bytes = sut.serializer().serialize(null, new Patient());
 
-        assertTrue(bytes.length > 0);
-    }
+    assertTrue(bytes.length > 0);
+  }
 }
